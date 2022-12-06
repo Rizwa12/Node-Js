1768014e08fd427430f34581b052442876541d96
Muhammad Rizwan <rizwanturk!2@gmail.com>
2022-12-06 18:04:53 +0500

    hello world serever complete

--- 2.hello world server/index.mjs
+++ 2.hello world server/index.mjs
@@ -1,11 +1,23 @@
  import express from 'express';
+ import cors from 'cors';
 const app = express()
+app.use(cors());
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/home', (req, res) => {
+  res.send('i am a home page')
 })
 
+app.get('/profile',(req,res)=>{
+  res.send('I am a profile a page')
+})
+app.get('/weather',(req,res)=>{
+  res.send({
+    weather:'Sunny',
+    temperature:'20',
+    city:'London'
+  })
+})
 app.listen(port, () => {
   console.log(`Hello world server listening on port ${port}`)
 })
